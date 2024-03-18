e074fe183018a4d8b6c0eb9662c38bf49b855c05
TallonsCZ <tallons@burningcube.eu>
2024-03-18 14:23:51 +0100

    Remove debugging message

--- src/main/java/me/tallonscze/guishop/utility/DynamicPriceUtility.java
+++ src/main/java/me/tallonscze/guishop/utility/DynamicPriceUtility.java
@@ -17,9 +17,6 @@ public class DynamicPriceUtility {
         double inBuy = ConfigUtility.getConfig().getInt("dynamic_economy.increase_buy", 1)/100.0;
         double inSell = ConfigUtility.getConfig().getInt("dynamic_economy.increase_sell", 1)/100.0;
         if(avargeBuy >= 1){
-            System.out.println(Math.round((iData.getBuy() * (1 + (avargeBuy*inBuy)))*100.0)/100.0);
-            System.out.println("inBuy is: " + inBuy);
-            System.out.println(((((1 + (avargeBuy*inBuy)))*100.0)/100.0));
             iData.setBuy(Math.round((iData.getBuy() * (1 + (avargeBuy*inBuy)))*100.0)/100.0);
             iData.setBuyed(0);
             GUIShop.invUtility.getInventory(inventory).setBuyed(0, slot);
